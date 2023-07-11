1df701ac691a0dcd47abd308e12617e64ef03a36
misaelorelio <misaelorelio@hotmail.com>
2023-07-10 21:42:16 -0300

    Projeto creado

---
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
+    private String longDescription;
+}
