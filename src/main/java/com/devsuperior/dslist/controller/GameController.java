91941793d18914283c7006641998ee40ce31b48a
misaelorelio <misaelorelio@hotmail.com>
2023-07-11 21:35:29 -0300

    novo controle e dto

--- src/main/java/com/devsuperior/dslist/controller/GameController.java
+++ src/main/java/com/devsuperior/dslist/controller/GameController.java
@@ -1,10 +1,12 @@
 package com.devsuperior.dslist.controller;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +18,14 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        var resultado = gameService.findById(id);
+        return resultado;
+    }
     @GetMapping
-    public List<GameMinDTO> findAll () {
+    public List<GameMinDTO> findAll() {
         var resultado = gameService.findAll();
         return resultado;
     }
