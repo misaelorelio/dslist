a91962e66a181bccebefceb571a58a700eff22de
misaelorelio <misaelorelio@hotmail.com>
2023-07-11 21:43:15 -0300

    lists, controler

---
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dslist.services;
+
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class GameListService {
+    @Autowired
+    private GameListRepository gameListRepository;
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    @Transactional(readOnly = true)
+    public List<GameListDTO> findAll() {
+        List<GameList> result = gameListRepository.findAll();
+        return result.stream().map(GameListDTO::new).toList();
+    }
+}
